5a8ffb754e376d1686e10516dc2e1dbceef184d2
w.h.sinn <w.h.sinn@gmail.com>
2019-03-26 19:32:44 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,5 @@
 class Hash
   def keys_of(arguments)
-    map {|key, value| args.include?(value) ? key : nil }.compact
+    map {|key, value| arguments.include?(value) ? key : nil }.compact
   end
 end
